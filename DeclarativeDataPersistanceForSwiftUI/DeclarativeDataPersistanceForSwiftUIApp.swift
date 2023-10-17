cf65188cc0ab4d32ae841ea22ac5eb13c97d292f
ram4ik <ram4ik@hotmail.com>
2023-10-16 21:26:12 -0700

    Initial Commit

---
+++ DeclarativeDataPersistanceForSwiftUI/DeclarativeDataPersistanceForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeclarativeDataPersistanceForSwiftUIApp.swift
+//  DeclarativeDataPersistanceForSwiftUI
+//
+//  Created by admin on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct DeclarativeDataPersistanceForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
