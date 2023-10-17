14354752b481eb7ea5df7491c1110860f0459c41
ram4ik <ram4ik@hotmail.com>
2023-10-16 21:36:29 -0700

    @State

--- DeclarativeDataPersistanceForSwiftUI/ContentView.swift
+++ DeclarativeDataPersistanceForSwiftUI/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter: Int = 0
+    @State private var isOn: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Toggle(isOn: $isOn, label: {
+                Text(isOn ? "Increment" : "Decrement")
+            })
+            .padding()
+            
+            Text("\(counter)")
+                .font(.largeTitle)
+                .padding()
+            
+            Button(isOn ? "Increment" : "Decrement") {
+                if isOn {
+                    counter += 1
+                } else {
+                    counter -= 1
+                }
+            }
         }
         .padding()
     }
